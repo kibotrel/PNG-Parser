cc1372be31fd6dbed1e50831f920a59b42ad7313
Kilian BOTREL <kibotrel@e3r5p1.42.fr>
2019-06-14 16:05:55 +0200

    Separated macros from the main header

---
+++ incs/macros.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   macros.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/14 16:03:44 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/06/14 16:05:19 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACRO_H
+# define MACRO_H
+
+/*
+**	Useful macros
+*/
+
+# define MAX_SIZE	8388608
+
+/*
+**	Error codes
+*/
+
+# define ERR_OPEN 	"\033[31;1mError :\033[0m Can't open file."
+# define ERR_FILE	"\033[31;1mError :\033[0m File is to large to be handled."
+# define ERR_SIZE	"\033[31;1mError :\033[0m Incorrect image dimensions."
+# define ERR_SIGN 	"\033[31;1mError :\033[0m Incorrect signature."
+# define ERR_COLOR	"\033[31;1mError :\033[0m Incorrect color type."
+# define ERR_DEPTH	"\033[31;1mError :\033[0m Incorrect bit depth."
+# define ERR_MALLOC "\033[31;1mError :\033[0m Can't allocate memory."
+# define ERR_HEADER	"\033[31;1mError :\033[0m Incorrect PNG Header."
+# define ERR_PRESET "\033[31;1mError :\033[0m Wrong bit depth - colortype pair."
+
+#endif
--- incs/png.h
+++ incs/png.h
@@ -1,7 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   png.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/14 16:03:23 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/06/14 16:04:28 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PNG_H
 # define PNG_H
-
-# define MAX_SIZE	8388608
 
 typedef struct		s_chunk
 {
@@ -45,15 +55,5 @@ void				header(char *buffer, t_control *file);
 void				clean(char *buffer, char *error, int code);
 void				setup(t_control *file, t_process *handler);
 void				fill_chunkname(char *chunk, char *buffer, int size);
-
-# define ERR_OPEN 	"\033[31;1mError :\033[0m Can't open file."
-# define ERR_FILE	"\033[31;1mError :\033[0m File is to large to be handled."
-# define ERR_SIZE	"\033[31;1mError :\033[0m Incorrect image dimensions."
-# define ERR_SIGN 	"\033[31;1mError :\033[0m Incorrect signature."
-# define ERR_COLOR	"\033[31;1mError :\033[0m Incorrect color type."
-# define ERR_DEPTH	"\033[31;1mError :\033[0m Incorrect bit depth."
-# define ERR_MALLOC "\033[31;1mError :\033[0m Can't allocate memory."
-# define ERR_HEADER	"\033[31;1mError :\033[0m Incorrect PNG Header."
-# define ERR_PRESET "\033[31;1mError :\033[0m Wrong bit depth - colortype pair."
 
 #endif
